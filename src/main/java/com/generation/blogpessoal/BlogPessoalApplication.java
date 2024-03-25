75f0c4b35b15155638bdb6f1bcb21a9a70df362e
Jonathan Garuti <jonathan_garuti@hotmail.com>
2024-03-25 17:01:39 -0300

    refatoração

--- src/main/java/com/generation/blogpessoal/BlogPessoalApplication.java
+++ src/main/java/com/generation/blogpessoal/BlogPessoalApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlogPessoal1Application {
+public class BlogPessoalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlogPessoal1Application.class, args);
+		SpringApplication.run(BlogPessoalApplication.class, args);
 	}
 
 }
